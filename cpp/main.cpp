11ac5f4646f8facaa6b9c89f7da2de881782e97a
Игорь <igor2kulach@gmail.com>
2025-12-26 12:13:15 +0300

    Добавлен fstream reader.

--- cpp/main.cpp
+++ cpp/main.cpp
@@ -1,18 +1,57 @@
 #include <iostream>
+#include <fstream>
+#include <limits>
+#include <filesystem>
 
-using num = int;
-using frac = double;
-using line = std::string;
+int main(int argc, char* argv[]) {
+    if (argc < 2) {
+        std::cout << "Использование: " << argv[0] << " <имя файла> <параметр> <строка>\n";
+        return 0;
+    }
 
-#define print std::cout
-#define n '\n'
-#define $ ;
+    std::string filename{argv[1]};
+    if (argc >= 3) {
+        std::string parametr{argv[2]}; 
+        if (parametr == "-reset") {
+            std::ofstream offile(filename);
+            std::cout << "Файл перезаписан.\n";
+            offile.close();
+            return 0;
+        }
 
-int main() {
-    num number{50}$
-    frac fractionalNumber{2.5}$
-    line hello{"привет"}$
-    print << hello << n $
-    print << number << ' ' << fractionalNumber << n $
-    return number $
+        if (parametr == "-write") {
+            if (argc >= 4) {
+                std::string userLine{argv[3]};
+                std::ofstream writeFile{filename, std::ios::app};
+                writeFile << '\n' << userLine;
+                std::cout << "Строка { " << userLine << " } записана.\n";
+                writeFile.close();
+                return 0;
+            }
+            else {
+                std::cout << "Не указана строка для записи!\n";
+                return 1;
+            }
+
+        }
+    }
+
+
+    if (!std::filesystem::exists(filename)) {
+        std::cout << "Подобного файла не существует.\n";
+        return 1;
+    }
+
+
+    std::ifstream file(filename);
+    if (!file) {
+        std::cout << "Не удалось прочитать!\n";
+        return 1;
+    }
+    std::string line{};
+    while (std::getline(file, line)) {
+        std::cout << line << '\n';
+    }
+    file.close();
+    return 0;
 }
